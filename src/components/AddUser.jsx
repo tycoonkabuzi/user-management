06f722ef2a934dd3f9b61c64478d902fdf5e0cb9
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-02-13 20:45:26 +0100

    Done

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+import { useUser } from "../context/UserContext";
+
 const AddUser = () => {
+  const { addUser } = useUser();
+  const [user, setUser] = useState([]);
+  const getDataFromForm = (e) => {
+    const theInput = e.target.name;
+    setUser((prev) => ({
+      ...prev,
+      id: Date.now(),
+      [theInput]: e.target.value,
+    }));
+  };
+
   return (
     <div>
       <h2>Add user</h2>
       <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Surname" />
-        <button>Add user</button>
+        <input
+          name="name"
+          type="text"
+          onChange={getDataFromForm}
+          placeholder="Name"
+          value={user.name}
+          required
+        />
+        <input
+          name="surname"
+          type="text"
+          onChange={getDataFromForm}
+          placeholder="Surname"
+          value={user.surname}
+          required
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            addUser(user);
+            setUser({ name: "", surname: "" });
+          }}
+        >
+          Add user
+        </button>
       </form>
     </div>
   );
